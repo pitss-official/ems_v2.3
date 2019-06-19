79fe8d4efe37080714b33e24525e03bf2948d0c3
Pawan Dubey <icare.pdubey@gmail.com>
2019-06-20 01:45:50 +0530

    system model development

---
+++ app/Http/Controllers/API/SmartCardController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Event;
+use App\Http\Controllers\Controller;
+use App\Team;
+use App\Enrollment;
+use App\User;
+use function GuzzleHttp\Promise\exception_for;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class SmartCardController extends Controller{
+
+    public function ticketPrice(){
+
+
+    }
+}
